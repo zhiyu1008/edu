0d65e976c4cc4c8ce55f8059f1400ea63324160d
alimo <2180244540@qq.com>
2022-08-15 19:47:11 +0800

    引入动画库

--- App.vue
+++ App.vue
@@ -16,5 +16,7 @@
 	/*每个页面公共css */
 	/* 引入公共样式表*/
 	@import url("@/common/index.css");
-	@import url("@/common/common.css")
+	@import url("@/common/common.css");
+	/* 引入动画库 */
+	@import url("@/common/animate.min.css");
 </style>
