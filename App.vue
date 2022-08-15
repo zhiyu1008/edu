946c85c0faad5a16967420030db023e43fc7312b
alimo <2180244540@qq.com>
2022-08-15 19:45:09 +0800

    引入全局的公共样式

--- App.vue
+++ App.vue
@@ -1,17 +1,20 @@
 <script>
 	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
+		// onLaunch: function() {
+		// 	console.log('App Launch')
+		// },
+		// onShow: function() {
+		// 	console.log('App Show')
+		// },
+		// onHide: function() {
+		// 	console.log('App Hide')
+		// }
 	}
 </script>
 
 <style>
 	/*每个页面公共css */
+	/* 引入公共样式表*/
+	@import url("@/common/index.css");
+	@import url("@/common/common.css")
 </style>
