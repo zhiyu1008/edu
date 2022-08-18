81abb5235667951a6513c77bc3b398518059cda7
alimo <2180244540@qq.com>
2022-08-18 20:51:39 +0800

    vuex的模块化拆分

--- store/index.js
+++ store/index.js
@@ -1,32 +1,13 @@
 import Vue from "vue"
 import Vuex from "vuex"
-// import {TOKEN_KEY, USER_KEY} from "@/config/storage-key"
 Vue.use(Vuex)
-const store = new Vuex.Store({
-	state : {
-		// token : uni.getStorageSync(TOKEN_KEY) || "",
-		// user : JSON.parse(uni.getStorageSync(USER_KEY) || "{}") 
-	},
-	getters : {
-		
-	},
-	mutations : {
-		// setUser(state, user){
-		// 	state.token = user.token
-		// 	state.user = user
-			
-		// 	uni.setStorageSync(TOKEN_KEY, user.token)
-		// 	uni.setStorageSync(USER_KEY, JSON.stringify(user))
-		// }
-	},
-	actions : {
-		// setUser({commit}, payload){
-		// 	commit("setUser", payload)
-		// }
-	},
-	modules : {
-		
+import user from '@/store/modules/user.js'
+import getters from './getters'
+
+const store=new Vuex.Store({
+	getters,
+	modules: {
+	  user
 	}
 })
-
 export default store
---
+++ store/modules/user.js
@@ -0,0 +1,16 @@
+import {
+  getItem,
+  removeItem,
+  setItem
+} from '../../utils/storage'
+export default {
+  namespaced: true,
+  state: {
+    
+  },
+  mutations: {
+    
+  },
+  actions: {
+  }
+}
